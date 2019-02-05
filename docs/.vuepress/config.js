818705e7b22a87cd76410ec460104700c7d70ef3
Heikki Kainulainen <hmkainul@gmail.com>
2019-02-05 10:23:55 +0100

    Add navbar

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    title: 'Heikki Kainulainen',
+    description: 'Programmer Heikki Kainulainen\'s Homepage. Getting Started with Java EE and React.',
+    themeConfig: {
+        search: false,
+        nav: [
+            { text: 'About Me', link: '/about-me/' },
+            { text: 'GitHub', link: 'https://github.com/hmkainul' }
+        ]
+    }
+}
