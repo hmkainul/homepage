919a6b2d1a617e1701ebb1c67fdad1def958c68f
Heikki Kainulainen <hmkainul@gmail.com>
2025-04-04 15:32:42 +0300

    Migrate from VuePress to VitePress

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,10 +1,12 @@
-module.exports = {
+// import { defineConfig } from 'vitepress'
+
+export default {
     title: 'Heikki Kainulainen',
     description: 'Programmer Heikki Kainulainen\'s Homepage. Getting Started with Java EE and React.',
     themeConfig: {
-        search: false,
+        // search: false,
         nav: [
-            { text: 'About Me', link: '/about-me/' },
+            { text: 'About Me', link: '/about-me' },
             { text: 'GitHub', link: 'https://github.com/hmkainul' }
         ]
     }
